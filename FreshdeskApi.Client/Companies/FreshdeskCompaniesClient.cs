f3ea5813081c0bec294086cacde784a218db2332
Kezia Shirazi <kezia.shirazi@metaswitch.com>
2020-10-28 15:30:17 +0000

    Add functionality to export and update companies

--- FreshdeskApi.Client/Companies/FreshdeskCompaniesClient.cs
+++ FreshdeskApi.Client/Companies/FreshdeskCompaniesClient.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using FreshdeskApi.Client.Companies.Models;
+using FreshdeskApi.Client.Companies.Requests;
+using FreshdeskApi.Client.Contacts.Requests;
 
 namespace FreshdeskApi.Client.Companies
 {
@@ -92,6 +95,81 @@ namespace FreshdeskApi.Client.Companies
             {
                 yield return company;
             }
+        }
+
+        /// <summary>
+        /// Export companies with the specified information
+        /// </summary>
+        ///
+        /// <param name="request">
+        /// All the fields of the companies to be retrieved.
+        /// </param>
+        ///
+        /// <param name="cancellationToken"></param>
+        ///
+        /// <returns>The ExportCsv object which contains an ID to be used</returns>
+        public async Task<ExportCsv> StartExportContactsAsync(
+            CompaniesExportRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request), "Request must not be null");
+
+            return await _freshdeskClient
+                .ApiOperationAsync<ExportCsv>(HttpMethod.Post, "/api/v2/companies/export", request, cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Check if an export has completed
+        /// </summary>
+        ///
+        /// <param name="export">
+        /// The export being checked.
+        /// </param>
+        ///
+        /// <param name="cancellationToken"></param>
+        ///
+        /// <returns>The ExportCsv object which contains an export url to be used</returns>
+        public async Task<ExportCsv> GetExportStatusAsync(
+            ExportCsv export,
+            CancellationToken cancellationToken = default)
+        {
+            if (export == null) throw new ArgumentNullException(nameof(export), "Export must not be null");
+            if (export.Id == null) throw new ArgumentNullException(nameof(export.Id), "Export Id must not be null");
+
+            return await _freshdeskClient
+                .ApiOperationAsync<ExportCsv>(HttpMethod.Get, $"/api/v2/contacts/export/{export.Id}", export, cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+
+        /// <summary>
+        /// Update a company with new details.
+        ///
+        /// c.f. https://developers.freshdesk.com/api/#update_company
+        /// </summary>
+        /// 
+        /// <param name="companyId">
+        /// The unique identifier for the contact.
+        /// </param>
+        ///
+        /// <param name="request">
+        /// The details about the company to update.
+        /// </param>
+        ///
+        /// <param name="cancellationToken"></param>
+        ///
+        /// <returns>The newly updated company</returns>
+        public async Task<Company> UpdateContactAsync(
+            long companyId,
+            UpdateCompanyRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request), "Request must not be null");
+
+            return await _freshdeskClient
+                .ApiOperationAsync<Company>(HttpMethod.Put, $"/api/v2/companies/{companyId}", request, cancellationToken)
+                .ConfigureAwait(false);
         }
     }
 }
