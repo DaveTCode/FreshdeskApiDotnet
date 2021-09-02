fddf0bd20a187fcd19701c4357c08c61a25cad71
Daniël Knippers <daniel.knippers@timetell.nl>
2021-09-02 08:46:45 +0200

    cosmetic text change

--- FreshdeskApi.Client/Contacts/Models/ListContact.cs
+++ FreshdeskApi.Client/Contacts/Models/ListContact.cs
@@ -5,9 +5,12 @@ namespace FreshdeskApi.Client.Contacts.Models
     public class ListContact : ContactBase
     {
         /// <summary>
-        /// Additional companies associated with the contact
+        /// Additional companies associated with the contact<br/>
+        /// 
+        /// NOTE: the list api has the limitation to only return the company ID and not the view_all_tickets propterty. 
+        /// This is an intentional limitation, see: https://community.freshworks.com/freshdesk-api-11012/api-v2-contacts-returning-only-the-id-s-in-other-companies-22089?postid=55712#post55712
         /// </summary>
         [JsonProperty("other_companies")]
-        public long[]? OtherCompanies { get; set; } //NOTE: ContactCompany would be logical but that is not supported in the API
+        public long[]? OtherCompanies { get; set; }
     }
 }
