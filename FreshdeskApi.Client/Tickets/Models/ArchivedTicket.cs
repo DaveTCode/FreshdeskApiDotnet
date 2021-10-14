f4c72e5f5448f13f4ad37e809df069d85c1dd510
Jan Trejbal <jan.trejbal@aviationexam.com>
2021-10-14 15:59:22 +0200

    Fix encoding of the ArchivedTicket

---
+++ FreshdeskApi.Client/Tickets/Models/ArchivedTicket.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+
+namespace FreshdeskApi.Client.Tickets.Models
+{
+    /// <summary>
+    /// Refers to a single archived ticket within Freshdesk.
+    ///
+    /// c.f. https://developers.freshdesk.com/api/#archive_tickets
+    /// </summary>
+    public class ArchivedTicket : Ticket
+    {
+        [JsonProperty("archived")]
+        public bool Archived { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, {nameof(Archived)}: {Archived}";
+        }
+    }
+}
