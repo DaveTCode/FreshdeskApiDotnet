5da35f18cea8486fa3fa3dbed4953e87d50a9e81
Robert Smits <robertsmits@hotmail.be>
2022-02-20 22:56:03 +0100

    Add properly formatted comments to CreateOutboundEmailRequest

--- FreshdeskApi.Client/Tickets/Requests/CreateOutboundEmailRequest.cs
+++ FreshdeskApi.Client/Tickets/Requests/CreateOutboundEmailRequest.cs
@@ -36,63 +36,93 @@ namespace FreshdeskApi.Client.Tickets.Requests
             Files = files;
         }
 
+        /// <summary>
         /// Name of the requester
+        /// </summary>
         [JsonProperty("name")]
         public string? RequesterName { get; }
 
+        /// <summary>
         /// Email address of the requester. If no contact exists with this email address in Freshdesk, it will be added as a new contact.
+        /// </summary>
         [JsonProperty("email")]
         public string? Email { get; }
 
+        /// <summary>
         /// Subject of the ticket. The default Value is null.
+        /// </summary>
         [JsonProperty("subject")]
         public string? Subject { get; }
 
+        /// <summary>
         /// Helps categorize the ticket according to the different kinds of issues your support team deals with. The default Value is null.
+        /// </summary>
         [JsonProperty("type")]
         public string? TicketType { get; }
 
-        /// Status of the ticket. The default Value is 2.
+        /// <summary>
+        /// Status of the ticket. The default Value is 5.
+        /// </summary>
         [JsonProperty("status")]
         public TicketStatus Status { get; }
 
+        /// <summary>
         /// Priority of the ticket. The default value is 1.
+        /// </summary>
         [JsonProperty("priority")]
         public TicketPriority Priority { get; }
 
+        /// <summary>
         /// HTML content of the ticket.
+        /// </summary>
         [JsonProperty("description")]
         public string? Description { get; }
 
+        /// <summary>
         /// Key value pairs containing the names and values of custom fields.
+        /// </summary>
         [JsonProperty("custom_fields")]
         public Dictionary<string, object>? CustomFields { get; }
 
+        /// <summary>
         /// Timestamp that denotes when the ticket is due to be resolved
+        /// </summary>
         [JsonProperty("due_by")]
         public DateTimeOffset? DueBy { get; }
 
+        /// <summary>
         /// ID of email config which is used for this ticket. (i.e., support@yourcompany.com/sales@yourcompany.com)
-        /// If product_id is given and email_config_id is not given, product's primary email_config_id will be set
+        /// </summary>
         [JsonProperty("email_config_id")]
         public long? EmailConfigId { get; }
 
+        /// <summary>
         /// Timestamp that denotes when the first response is due
+        /// </summary>
         [JsonProperty("fr_due_by")]
         public DateTimeOffset? FirstResponseDueBy { get; }
 
+        /// <summary>
         /// ID of the group to which the ticket has been assigned. The default value is the ID of the group that is associated with the given email_config_id
+        /// </summary>
         [JsonProperty("group_id")]
         public long? GroupId { get; }
 
+        /// <summary>
         /// Tags that have been associated with the ticket
+        /// </summary>
         [JsonProperty("tags")]
         public string[]? Tags { get; }
 
+        /// <summary>
         /// Email address added in the 'cc' field of the incoming ticket email
+        /// </summary>
         [JsonProperty("cc_emails")]
         public string[]? CcEmails { get; }
 
+        /// <summary>
+        /// Ticket attachments. The total size of these attachments cannot exceed 20MB.
+        /// </summary>
         [JsonIgnore, Multipart(Name = "attachments")]
         public IEnumerable<FileAttachment>? Files { get; }
 
