5a8e65cb698c46055024bd2eafa0b68852fa3b34
Jan Trejbal <jan@trejbal.land>
2023-11-14 16:07:19 +0100

    Add AddFreshdeskApiClient overload with OptionsBuilder argument (#217)

--- FreshdeskApi.Demo/Program.cs
+++ FreshdeskApi.Demo/Program.cs
@@ -9,22 +9,15 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(static configurationBuilder =>
-    {
-        configurationBuilder.AddJsonFile("appsettings.json5", optional: false);
-        configurationBuilder.AddJsonFile("user.appsettings.json5", optional: false);
-    })
-    .ConfigureServices(static (hostBuilder, services) =>
-    {
-        services.AddFreshdeskApiClient(options =>
-        {
-            var freshdeskConfiguration = hostBuilder.Configuration.GetSection("Freshdesk");
-            options.FreshdeskDomain = freshdeskConfiguration[nameof(options.FreshdeskDomain)]
-                                      ?? throw new NullReferenceException($"{nameof(options.FreshdeskDomain)} not specified in appsettings.json5:Freshdesk");
-            options.ApiKey = freshdeskConfiguration[nameof(options.ApiKey)]
-                             ?? throw new NullReferenceException($"{nameof(options.ApiKey)} not specified in appsettings.json5:Freshdesk");
-        });
-    })
+    .ConfigureAppConfiguration(static configurationBuilder => configurationBuilder
+        .AddJsonFile("appsettings.json5", optional: false)
+        .AddJsonFile("user.appsettings.json5", optional: false)
+    )
+    .ConfigureServices(static services => services.AddFreshdeskApiClient(
+        optionsBuilder => optionsBuilder
+            .BindConfiguration(configSectionPath: "Freshdesk")
+            .ValidateOnStart()
+    ))
     .Build();
 
 using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(20));
