6cc4ad21fdc138c6c8d4391c37f64e1750ede087
Jan Trejbal <jan@trejbal.land>
2023-11-23 14:13:11 +0100

    Add IRequestWithAdditionalMultipartFormDataContent

---
+++ FreshdeskApi.Client/IRequestWithAdditionalMultipartFormDataContent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace FreshdeskApi.Client;
+
+public interface IRequestWithAdditionalMultipartFormDataContent
+{
+    IEnumerable<(HttpContent HttpContent, string Name)> GetAdditionalMultipartFormDataContent();
+}
