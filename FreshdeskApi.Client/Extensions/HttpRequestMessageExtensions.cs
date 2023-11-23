b4e4325f0c365c78c325aaf033f74b4a821f0cf5
Jan Trejbal <jan@trejbal.land>
2023-11-23 14:15:14 +0100

    Drop MapToWithoutCustomFields

--- FreshdeskApi.Client/Extensions/HttpRequestMessageExtensions.cs
+++ FreshdeskApi.Client/Extensions/HttpRequestMessageExtensions.cs
@@ -19,53 +19,6 @@ public static class HttpRequestMessageExtensions
         return content;
     }
 
-    public static CreateTicketRequestWithoutCustomFields MapToWithoutCustomFields(this CreateTicketRequest originalRequest)
-    {
-        var newRequest = new CreateTicketRequestWithoutCustomFields(
-             originalRequest.Status,
-             originalRequest.Priority,
-             originalRequest.Source,
-             originalRequest.Description,
-             originalRequest.RequesterName,
-             originalRequest.RequesterId,
-             originalRequest.Email,
-             originalRequest.FacebookId,
-             originalRequest.PhoneNumber,
-             originalRequest.TwitterId,
-             originalRequest.UniqueExternalId,
-             originalRequest.ResponderId,
-             originalRequest.CcEmails,
-             originalRequest.DueBy,
-             originalRequest.EmailConfigId,
-             originalRequest.FirstResponseDueBy,
-             originalRequest.GroupId,
-             originalRequest.ProductId,
-             originalRequest.Tags,
-             originalRequest.CompanyId,
-             originalRequest.Subject,
-             originalRequest.TicketType,
-             originalRequest.ParentTicketId,
-             originalRequest.Files);
-
-        return newRequest;
-    }
-
-    public static void AddCustomFields(this HttpContent content, Dictionary<string, object>? customFields)
-    {
-        if (content is MultipartFormDataContent multipartContent && customFields?.Any() == true)
-        {
-            foreach (var customField in customFields)
-            {
-                var key = $"custom_fields[{customField.Key}]";
-                var value = customField.Value?.ToString() ?? string.Empty;
-
-#pragma warning disable CA2000
-                multipartContent.Add(new StringContent(value), key);
-#pragma warning restore CA2000
-            }
-        }
-    }
-
     public static StringContent CreateJsonContent<TBody>(
         this TBody body
     ) where TBody : class => new(
